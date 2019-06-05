8196b5a64be34c0b3f96f4a029d4094a9e22b00a
Jaroslav Khorishchenko <websnipter@gmail.com>
2019-06-05 16:11:52 +0300

    :sparkles: CORS added

--- utils/resp.ts
+++ utils/resp.ts
@@ -1,24 +1,34 @@
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Credentials': true,
+};
+
 export const okResp = <T = any>(data: T) => ({
   statusCode: 200,
+  headers: {...corsHeaders},
   body: JSON.stringify(data),
 });
 
 export const serverErrResp = (data: string) => ({
   statusCode: 503,
+  headers: {...corsHeaders},
   body: JSON.stringify({error: data}),
 });
 
 export const notFoundResp = (data?: string) => ({
   statusCode: 404,
+  headers: {...corsHeaders},
   body: JSON.stringify({error: data || ''}),
 });
 
 export const paramMissedResp = (paramName: string) => ({
   statusCode: 422,
+  headers: {...corsHeaders},
   body: JSON.stringify({error: `"${paramName}" param missed`}),
 });
 
 export const paramWrongFormatResp = (paramName: string) => ({
   statusCode: 422,
+  headers: {...corsHeaders},
   body: JSON.stringify({error: `Wrong "${paramName}" format`}),
 });
